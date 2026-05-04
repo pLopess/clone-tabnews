eb1cc216d47a38271ac046d7284f0628144a8630
Pedro Lopes <87770252+pLopess@users.noreply.github.com>
2026-05-04 15:50:28 +0000

    corrigindo texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <><h1>Te amo, bonitinha!</h1><h2>Eduardinha, não fica triste.É uma etapa da sua escalada e no final vai tudo valer a pena. Não desiste e conta comigo!</h2></>
+    return <><h1>Te amo, bonitinha!</h1><h2>Eduardinha, não fica triste. É uma etapa da sua escalada e no final vai tudo valer a pena. Não desiste e conta comigo!</h2></>
 }
 
 export default Home;
